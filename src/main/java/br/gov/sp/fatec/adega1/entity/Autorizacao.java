03aafc24544d699500a6eb49f7760f5bac6fa39a
sandrotoline <sandro.toline@gmail.com>
2021-08-20 18:44:51 +0000

    Teste

---
+++ src/main/java/br/gov/sp/fatec/adega1/entity/Autorizacao.java
@@ -0,0 +1,10 @@
+package br.gov.sp.fatec.adega1.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "aut_autorizacao")
+public class Autorizacao {
+    
+}
