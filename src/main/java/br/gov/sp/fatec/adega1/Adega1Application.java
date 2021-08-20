c658ea740ae00a98fcd48f5e6cbfbafb469c6e0e
sandrotoline <sandro.toline@gmail.com>
2021-08-20 14:17:55 -0300

    initial commit

---
+++ src/main/java/br/gov/sp/fatec/adega1/Adega1Application.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.adega1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Adega1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Adega1Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/gov/sp/fatec/adega1/Adega1ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.adega1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Adega1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
