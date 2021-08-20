026aff3838d16698865c0947ae881031a3a3ef1f
sandrotoline <sandro.toline@gmail.com>
2021-08-20 18:11:55 +0000

    pom

---
+++ bd.sql
@@ -0,0 +1,22 @@
+create schema adega;
+
+use adega;
+
+create 'user'@'localhost' identified by 'adm123';
+
+grant select, insert, delete, update on adega.* to user@'localhost';
+
+create table usuario (
+  usr_id bigint unsigned not null auto_increment,
+  usr_nome varchar(20) not null,
+  usr_senha varchar(50) not null,
+  primary key (usr_id),
+  unique key uni_usuario_nome (usr_nome)
+);
+
+create table aut_autorizacao (
+  aut_id bigint unsigned not null auto_increment,
+  aut_nome varchar(20) not null,
+  primary key (aut_id),
+  unique key uni_aut_nome (aut_nome)
+);
