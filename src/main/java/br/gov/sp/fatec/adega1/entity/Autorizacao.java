e5efe8425e0bbab50af2e715c435e67dc5460b82
sandrotoline <sandro.toline@gmail.com>
2021-08-23 13:14:20 +0000

    Teste

--- src/main/java/br/gov/sp/fatec/adega1/entity/Autorizacao.java
+++ src/main/java/br/gov/sp/fatec/adega1/entity/Autorizacao.java
@@ -2,9 +2,25 @@ package br.gov.sp.fatec.adega1.entity;
 
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+
+
 
 @Entity
 @Table(name = "aut_autorizacao")
 public class Autorizacao {
-    
+
+  @Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Basic(optional = false)
+	@Column(name = "aut_id")
+	private Integer autId;
+
+  @Column(name = "aut_nome")
+	private String autNome;
+
 }
