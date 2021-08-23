096909782182fbb1128459633ca834788d2d489c
sandrotoline <sandro.toline@gmail.com>
2021-08-23 15:23:02 +0000

    Atualizacao BD

--- bd.sql
+++ bd.sql
@@ -22,3 +22,31 @@ create table aut_autorizacao (
   primary key (aut_id),
   unique key uni_aut_nome (aut_nome)
 );
+
+create table uau_usuario_autorizacao (
+usr_id bigint unsigned not null,
+aut_id bigint unsigned not null,
+primary key (usr_id, aut_id),
+foreign key uau_usr_fk (usr_id) references usr_usuario (usr_id) on delete restrict on update cascade,
+foreign key uau_aut_fk (aut_id) references aut_autorizacao (aut_id) on delete restrict on update cascade
+);
+
+create table fabricante(
+  fb_id bigint unsigned not null,
+  fb_nome varchar(50) not null,
+  fb_pais varchar(50) not null, 
+  primary key (fb_id),
+   unique key uni_fb_nome (fb_nome)
+)
+
+create table bebida(
+  bed_id bigint unsigned not null,
+  beb_nome varchar(50) not null,
+  beb_volume varchar(10) not null,
+  beb_tipo varchar(30) not null,
+  beb_descricao varchar(500) not null,
+  fb_beb_id bigint unsigned not null,
+  primary key (bed_id),
+  foreign key beb_fab_fk (fb_beb_id) references fabricante(fb_beb_id) on delete restrict on update cascade,
+  unique key uni_beb_nome (beb_nome)
+)
--- src/main/java/br/gov/sp/fatec/adega1/entity/Usuario.java
+++ src/main/java/br/gov/sp/fatec/adega1/entity/Usuario.java
@@ -26,4 +26,38 @@ public class Usuario {
 
     @Column(name = "usr_senha")
     private String senha;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    
 }
